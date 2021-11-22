14773ce04d73bb5e612e57db6ca5cb0c627c29f5
Giuseppe Vigneri <geppe1990@gmail.com>
2021-11-22 23:48:23 +0100

    puntatori

--- Modulo A Fondamenti di Informatica/09 Tipi di Dato strutturati/esercizi/ciagia.c
+++ Modulo A Fondamenti di Informatica/09 Tipi di Dato strutturati/esercizi/ciagia.c
@@ -6,15 +6,36 @@
 
 #include <stdio.h>
 
-void is_vocale() {
+int is_vocale(char c) {
 	int risultato = 0, i;
 	char vocali[] = "aeiou";
 
 	for(i=0; vocali[i] != '\0'; i++) {
-		printf("i: %d - %c\n",i, vocali[i]);
+		if(vocali[i] == c) {
+			risultato = 1;
+		}
 	}
+
+	return risultato;
 }
 
 main() {
-	is_vocale();
+	char s1[] = "fascia";
+	int i, vocale = 0;
+
+	for(i=0; s1[i] != '\0'; i++) {
+		if(s1[i] == 'c' || s1[i] == 'g') {
+			if(s1[i+1] == 'i' && s1[i+2] == 'a') {
+				vocale = is_vocale(s1[i-1]);
+
+				if(vocale) {
+					s1[i+2] = 'e';
+				} else {
+					s1[i+1] = 'e';
+					s1[i+2] = '\0';
+				}
+			}
+		}
+	}
+	printf("%s\n", s1);
 }
